485c2db2f689cf46700c46aa5df8db7e3a972fd1
HalloWelt42 <ds@texeln.de>
2021-01-06 00:33:52 +0100

    format

--- src/controller/LinkedList.php
+++ src/controller/LinkedList.php
@@ -12,10 +12,12 @@ class LinkedList
     private INode|null $linked_list;
     private INode|null $node;
 
+
     public function __construct( Node $linked_list = null )
     {
         $this -> linked_list = $linked_list;
     }
+
 
     public function insertAtLast( string $data )
     {
@@ -31,12 +33,14 @@ class LinkedList
 
     }
 
+
     public function interAtFirst( string $data ):self{
         $node = new Node($data );
         $node->setChild($this->linked_list);
         $this->linked_list = $node;
         return $this;
     }
+
 
     public function get():?Node{
         return $this->linked_list;
